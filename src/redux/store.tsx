09a7368340c3971dbc77832578c8418662316439
Abel Aron Amieva <abelamieva@gmail.com>
2022-12-01 19:09:20 -0300

    se actualizo el store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './feautures/auth/authSlice';
+import { authSlice } from './slices/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authSlice.reducer
   },
 })
 
