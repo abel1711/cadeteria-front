40647bcc57b14d4a5451975d1e9557c12af4c7b5
Abel Aron Amieva <abelamieva@gmail.com>
2022-12-01 19:54:34 -0300

    se establecion los errores en el slice

--- src/redux/slices/auth/authSlice.tsx
+++ src/redux/slices/auth/authSlice.tsx
@@ -1,17 +1,21 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RespLogin, Usuario } from '../../../interfaces';
 import { RootState } from '../../store'
+import { ErrorAuth } from '../../../interfaces/interface';
 
 interface authState {
     token: string | null;
     isLoading: boolean;
     usuario: Usuario | null;
+    error: ErrorAuth | null;
+
 }
 
 const initialState: authState = {
     token: null,
     isLoading: false,
-    usuario: null
+    usuario: null,
+    error: null,
 }
 
 export const authSlice = createSlice({
@@ -24,7 +28,12 @@ export const authSlice = createSlice({
         setUsuario: (state, action: PayloadAction<RespLogin>) => {
             state.token = action.payload.token;
             state.usuario = action.payload.usuario;
-            state.isLoading= false;
+            state.isLoading = false;
+            state.error = null;
+        },
+        setErrors: (state, action: PayloadAction<ErrorAuth>) => {
+            state.isLoading = false;
+            state.error = action.payload
         },
         resetAll: () => {
             return initialState
@@ -32,6 +41,6 @@ export const authSlice = createSlice({
     },
 })
 
-export const { setUsuario, resetAll, startLoadingUsuario } = authSlice.actions
+export const { setUsuario, resetAll, startLoadingUsuario, setErrors } = authSlice.actions
 
 export const selectAuth = (state: RootState) => state.auth
