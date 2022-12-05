56ddc046688d5473cb35e72132bb28858e0bda69
Abel Aron Amieva <abelamieva@gmail.com>
2022-12-05 17:12:48 -0300

    se agrego el loading al store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authSlice } from './slices/auth/authSlice';
+import { loadingSlice } from './slices/loading/loadingSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authSlice.reducer
+    auth: authSlice.reducer,
+    loading: loadingSlice.reducer
   },
 })
 
