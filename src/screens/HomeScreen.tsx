0c6f40cd1e97000ac7ec1eaf49ad4b4f285e7210
Abel Aron Amieva <abelamieva@gmail.com>
2022-12-05 19:45:22 -0300

    se obtiene la informacion de la cadeteria en el Home

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react"
+import { useAppDispatch } from '../redux/hooks';
+import { startGetDataPage } from '../redux/slices/data-page/data-pageThunks';
 
 export const HomeScreen = () => {
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(startGetDataPage())
+  }, [])
+  
   return (
     <div>HomeScreen</div>
   )
