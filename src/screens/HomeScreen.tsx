8c4659ad8269a976e5a6fa86e5c1f4fa82f7cd62
Abel Aron Amieva <abelamieva@gmail.com>
2022-12-06 19:30:04 -0300

    se trabajo el el homeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,57 @@
 import { useEffect } from "react"
-import { useAppDispatch } from '../redux/hooks';
+import { CssBaseline, Typography, Box, Grid, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { Container } from "@mui/system";
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { startGetDataPage } from '../redux/slices/data-page/data-pageThunks';
+import { Card, CardServicios, NavbarHome } from "../components";
 
 export const HomeScreen = () => {
 
   const dispatch = useAppDispatch();
+  const data = useAppSelector(state => state.dataPage)
 
   useEffect(() => {
     dispatch(startGetDataPage())
   }, [])
-  
+
   return (
-    <div>HomeScreen</div>
+    <>
+      <NavbarHome />
+      <Container component={'main'} maxWidth='lg' style={{
+        // backgroundColor: 'red'
+      }}>
+        <CssBaseline />
+
+        <Grid container spacing={1} mt={2}>
+          
+          <Grid item  sm={6}>
+            <Card
+              titulo="¿Quienes Somos?"
+              text={data.about}
+            />
+          </Grid>
+
+          <Grid item  sm={6} >
+            <Card
+              titulo="Nuestra misión"
+              text={data.mision}
+            />
+          </Grid>
+
+          <Grid item  sm={6}>
+            <Card
+              titulo="Nuestros objetivos"
+              text={data.objetivos}
+            />
+          </Grid>
+          <Grid item  sm={6}>
+            <CardServicios servicios={data.servicios}/>
+          </Grid>
+
+
+        </Grid>
+
+      </Container>
+    </>
   )
 }
