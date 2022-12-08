3da530c23e7e3ba9bc7a1ce8ed7dc449741340c0
Abel Aron Amieva <abelamieva@gmail.com>
2022-12-08 19:00:22 -0300

    se linkea el navbar

--- src/components/NavbarHome.tsx
+++ src/components/NavbarHome.tsx
@@ -1,15 +1,19 @@
 import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useTheme } from '@mui/material/styles';
+
+import { Link } from 'react-router-dom';
 
 
 export const NavbarHome = () => {
 
+    const theme = useTheme();
 
     return (
         <div>
             <AppBar >
                 <Toolbar>
-                    <IconButton
+                    {/* <IconButton
                         size="large"
                         edge="start"
                         color="inherit"
@@ -17,11 +21,28 @@ export const NavbarHome = () => {
                         sx={{ mr: 2 }}
                     >
                         <MenuIcon />
-                    </IconButton>
+                    </IconButton> */}
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         MF - Gestiones
                     </Typography>
-                    <Button color="inherit">Login</Button>
+
+                    <Button color="inherit">
+                        <Link to="/login" style={{
+                            textDecoration: 'none',
+                            color: 'inherit'
+                        }}>
+                            Ingresar
+                        </Link>
+                    </Button>
+
+                    <Button color="inherit">
+                    <Link to="/registro" style={{
+                            textDecoration: 'none',
+                            color:'inherit'
+                        }}>
+                            Regístrate
+                        </Link>
+                    </Button>
                 </Toolbar>
             </AppBar>
         </div>
