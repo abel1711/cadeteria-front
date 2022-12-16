2c9a9e394de73c8ed1d06c81323a2074646d983a
Abel Aron Amieva <abelamieva@gmail.com>
2022-12-16 16:09:01 -0300

    se crea archivo index

---
+++ src/router/index.ts
@@ -0,0 +1,7 @@
+export { RutasCadete } from './RutasCadete';
+export { RutasCliente } from './RutasCliente';
+export { RutasAdministrador } from './RutasAdministrador';
+export { Rutas } from "./Rutas";
+export { RutasPrivadas } from './RutasPrivadas';
+export { RutasPublicas } from './RutasPublicas';
+
---
+++ src/screens/index.ts
@@ -0,0 +1,7 @@
+export { LoginScreen } from './LoginScreen';
+export { RegistroScreen } from './RegistroScreen';
+export { ErrorPage } from './ErrorScreen';
+export { HomeScreen } from './HomeScreen';
+export { PaginaAdministrador } from './PaginaAdministrador';
+export { PaginaCliente } from './PaginaCliente';
+export { PaginaCadete } from './PaginaCadete';
