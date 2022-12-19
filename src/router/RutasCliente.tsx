be55922147dd07af898c7dcf96066b20d3d4545c
Abel Aron Amieva <abelamieva@gmail.com>
2022-12-19 17:52:03 -0300

    se crearon las rutas para el cliente

--- src/router/RutasCliente.tsx
+++ src/router/RutasCliente.tsx
@@ -1,11 +1,19 @@
 
+import { Route, Routes } from "react-router-dom"
 import { ClienteNavbar } from "../components/cliente/ClienteNavbar"
+import { MisOrdenesCliente } from "../components/cliente/MisOrdenesCliente"
+import { NuevaOrdenCliente } from '../components/cliente/NuevaOrdenCliente';
 
 export const RutasCliente = () => {
   
   return (
     <>
       <ClienteNavbar />
+      <Routes>
+        <Route path="mis-ordenes" element={<MisOrdenesCliente />}/>
+        <Route path="nueva-orden" element={<NuevaOrdenCliente />}/>
+        <Route path="mi-perfil" element={<MisOrdenesCliente />}/>
+      </Routes>
     </>
   )
 }
