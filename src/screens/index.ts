8bee835204cba475db907250d512050ae3ecd484
Abel Aron Amieva <abelamieva@gmail.com>
2022-12-23 19:10:39 -0300

    se actualizo el index

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,4 +1,4 @@
-export { LoginScreen } from './LoginScreen';
+export { LoginScreen } from './login/LoginScreen';
 export { RegistroScreen } from './RegistroScreen';
 export { ErrorPage } from './ErrorScreen';
 export { HomeScreen } from './HomeScreen';
