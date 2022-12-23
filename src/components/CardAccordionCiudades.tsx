e1d520fe11abf8d735d6c0f18d6b122e3e8d23ff
Abel Aron Amieva <abelamieva@gmail.com>
2022-12-23 19:07:30 -0300

    refac

--- src/components/CardAccordionCiudades.tsx
+++ src/components/CardAccordionCiudades.tsx
@@ -2,40 +2,40 @@ import { Typography, Accordion, AccordionSummary, AccordionDetails, List, ListIt
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 interface Props {
-    ciudades : string[];
+    ciudades: string[];
 }
 
-export const CardAccordionCiudades = ({ciudades}:Props) => {
+export const CardAccordionCiudades = ({ ciudades }: Props) => {
 
-  return (
-    <Accordion>
-    <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1a-content"
-        id="panel1a-header"
-    >
-        <Typography
-            variant="h6" component="span"
-        >Zona de trabajo</Typography>
-    </AccordionSummary>
-    <AccordionDetails>
-        <List
-            sx={{ width: '100%', bgcolor: 'background.paper' }}
-            component="span"
-            aria-labelledby="nested-list-subheader"
-        >
-            {
-                ciudades.map(( ciudad ) => (
-                    <ListItemText
-                        key={ciudad}
-                        primary={`${ciudad.split(',')[0]} - ${ciudad.split(',')[1]}`}
-                    />
+    return (
+        <Accordion>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+            >
+                <Typography
+                    variant="h6" component="span"
+                >Zona de trabajo</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <List
+                    sx={{ width: '100%', bgcolor: 'background.paper' }}
+                    component="span"
+                    aria-labelledby="nested-list-subheader"
+                >
+                    {
+                        ciudades.map((ciudad) => (
+                            <ListItemText
+                                key={ciudad}
+                                primary={`${ciudad.split(',')[0]} - ${ciudad.split(',')[1]}`}
+                            />
 
-                ))
-            }
+                        ))
+                    }
 
-        </List>
-    </AccordionDetails>
-</Accordion>
-  )
+                </List>
+            </AccordionDetails>
+        </Accordion>
+    )
 }
