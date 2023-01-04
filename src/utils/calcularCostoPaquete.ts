31ba8aa63c85b5ed1fcb3571dc08e77349fdab1d
Abel Aron Amieva <abelamieva@gmail.com>
2023-01-04 19:06:49 -0300

    avances en el calculo del costo

--- src/utils/calcularCostoPaquete.ts
+++ src/utils/calcularCostoPaquete.ts
@@ -1,11 +1,30 @@
 import { InfoPaquete } from '../interfaces/interface'
 
-export const calcularCostoPaquete = (infoPaquete: InfoPaquete, precioBase : String) => {
-    
-    const { alto, ancho, largo, peso } = infoPaquete;
+export const calcularCostoPaquete = (infoPaquete: InfoPaquete, precioBase: String) => {
+
+    const { alto, ancho, largo, peso, bultos } = infoPaquete;
 
     const volumen = Number(alto) * Number(ancho) * Number(largo);
+    const precioNumber = Number(precioBase);
+    const bultosNumber = Number(bultos);
+    const volumenTotal = volumen * bultosNumber;
+    const costoSegunVolumen =
+        (volumen <= 15625) //25*25*25
+            ? precioNumber
+            : (volumen > 15625 && volumen <= 31250) //25*25*50
+                ? precioNumber * 1.5
+                : (volumen > 31250 && volumen <= 42875) //35*35*35
+                    ? precioNumber * 2
+                    : (volumen > 42875 && volumen <= 64000) //40*40*40
+                        ? precioNumber * 2.5
+                        : 0 // FALTA MANEJAR LA EXCEPCION CUANDO EL VOLUMEN DEL PAQUETE ES MUY GRANDE
 
+    const costoMasBultos = //se calcula sumandole el 50% a partir del segundo bulto
+        (bultosNumber > 1) ? costoSegunVolumen * (0.5 + (bultosNumber * 0.5)) : costoSegunVolumen;
+    // volumen maximo de la camioneta en centimetros es: 2200000
     console.log(volumen)
+    console.log('costo segun el volumen: ', costoSegunVolumen)
+    console.log('volumen total: ',volumenTotal)
+    console.log('costo final: ', costoMasBultos)
 
 }
