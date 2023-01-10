7f28ec58ac2eaac8b55a333ad069332e6ae39b2a
Abel Aron Amieva <abelamieva@gmail.com>
2023-01-10 18:13:52 -0300

    se esta creando el thunk pára el post de la orden

--- src/redux/slices/nueva-orden/nueva-ordenThunks.tsx
+++ src/redux/slices/nueva-orden/nueva-ordenThunks.tsx
@@ -1,9 +1,9 @@
 import { store } from '../../store';
 import { removeLoading, setLoading } from '../loading/loadingSlice';
 import { dataPageAPI } from '../../../api/dataPageAPI';
-import { FormConDomicilioRemitente, FormSinDomicilioRemitente } from '../../../interfaces/interface';
+import { FormConDomicilioRemitente } from '../../../interfaces/interface';
 import { calcularCostoPaquete } from '../../../utils/calcularCostoPaquete';
-import { setNuevaOrden } from './nueva-ordenSlice';
+import { setNuevaOrden, setPrecioOrden } from './nueva-ordenSlice';
 
 
 
@@ -18,13 +18,34 @@ export const startNuevaOrden = ( formulario : FormConDomicilioRemitente )=>{
             const {data} = await dataPageAPI.get('/precios');
 
             const precioBase = formulario.tipoDeOrden == '@PACK' ? data.pack.precioBase : data.moto.precioBase;
+            const precioFinal = `${calcularCostoPaquete(formulario.infoPaquete, precioBase)}`;
 
-            formulario.infoPaquete.costo = `${calcularCostoPaquete(formulario.infoPaquete, precioBase)}`
 
-            dispatch(setNuevaOrden(formulario))
+            dispatch(setNuevaOrden(formulario));
+            dispatch(setPrecioOrden(precioFinal));
             dispatch( removeLoading())
         } catch (errors: any) {
-            console.log(errors.response.data.errors)
+            console.log(errors)
+            dispatch( removeLoading())
+
+        }
+
+
+    }
+}
+
+export const startPostNuevaOrden = ( )=>{
+    return async( dispatch: typeof store.dispatch , getState: typeof store.getState )=>{
+
+        dispatch( setLoading() );
+
+        
+        try {
+            const state = getState().nuevaOrden;
+            console.log(state)
+            dispatch( removeLoading())
+        } catch (errors: any) {
+            console.log(errors)
             dispatch( removeLoading())
 
         }
